64162f58761dbcb6e6f31a483e0e5b9d5673d5d3
Andy Shen <andyshenproper@gmail.com>
2019-02-09 20:30:28 -0800

    Ignore .class for compilation

--- FoodPrints/src/main/java/com/inhostudios/visionapitester/FoodPrints.java
+++ FoodPrints/src/main/java/com/inhostudios/visionapitester/FoodPrints.java
@@ -1,12 +1,28 @@
 package com.inhostudios.visionapitester;
 
+import com.github.sarxos.webcam.Webcam;
+
+import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
 public class FoodPrints {
 
-    public static void main(String[] args) throws Exception{
-        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\apple.jpg";
+    public static void main(String[] args){
+
+        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\image.jpg";
+
+        Webcam webcam = new Webcam.getDefault();
+        webcam.open();
+        BufferedImage image = webcam.getImage();
+        try {
+            ImageIO.write(image, "JPG", new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         ImageInterpreter imageInterpreter = new ImageInterpreter(path);
         ArrayList<String> outputs = imageInterpreter.getOutputs();
