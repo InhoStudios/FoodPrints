1727d748cf2aabd213b5e8a89a12c39d7d150985
harrychuang <vrjgik5@gmail.com>
2019-02-10 00:46:15 -0800

    "DataExtraction is working! Now going to parse array of objects to recipes"

--- FoodPrints/src/main/java/com/inhostudios/visionapitester/DataExtraction.java
+++ FoodPrints/src/main/java/com/inhostudios/visionapitester/DataExtraction.java
@@ -1,6 +1,7 @@
 package com.inhostudios.visionapitester;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.stream.JsonReader;
 
@@ -10,15 +11,17 @@ import java.io.InputStreamReader;
 import java.io.StringReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
 
 
 enum Diet {
-    // TODO: not sure if this is the correct implementation
-    BALANCED("balanced"),
-    HIGH_PROTEIN("high-protein"),
+    BALANCED("balanced"),         // TESTED
+    HIGH_PROTEIN("high-protein"), // TESTED
+    LOW_FAT("low-fat"),           // TESTED
+    LOW_CARB("low-carb"),         // TESTED
+
+    // NOT WORKING
     HIGH_FIBER("high-fiber"),
-    LOW_FAT("low-fat"),
-    LOW_CARB("low-carb"),
     LOW_SODIUM("low-sodium");
 
     private final String text;
@@ -33,6 +36,62 @@ enum Diet {
     }
 }
 
+class RecipeQuery {
+    private String recipeAppID = "&app_id=ab5047e9";
+    private String recipeAppKey = "&app_key=391d21268595ea4650e3d0f9039af88f";
+
+    private String q; // query text
+    private String appID = recipeAppID;     // TESTED
+    private String appKey = recipeAppKey;   // TESTED
+    private String from = "&from=0";        // TESTED
+    private String to = "&to=3";            // TESTED
+    private String diet = "";               // TESTED
+    private String calories = "";           // TESTED
+    private String time = "";               // TESTED
+    private String exluded = "";            // TESTED
+
+    public RecipeQuery(String oneKeyWord){
+        this.setQ(oneKeyWord);
+    }
+
+
+    public void setQ(String q) {
+        this.q = "q=" + q;
+    }
+
+
+    public void setDiet(String diet) {
+        this.diet = "&diet=" + diet;
+    }
+
+    public void setCalories(int minCal, int maxCal) {
+        this.calories = "&calories=" + minCal + "-" + maxCal;
+    }
+
+    public void setTime(int min) {
+        this.time = "&time=" + min + "%2B";
+    }
+
+    public void setTime(int min, int max) {
+        this.time = "&time=" + min + "-" + max;
+    }
+
+    public void setExluded(String excludeFood) {
+        this.exluded = "&excluded=" + excludeFood;
+    }
+
+    public String toURL() {
+        String testing = "https://api.edamam.com/search?q=chicken&app_id=ab5047e9" +
+                "&app_key=391d21268595ea4650e3d0f9039af88f&from=0&to=3&health=alcohol-free&calories=591-722";
+
+
+        String result = "https://api.edamam.com/search?" + q + appID +
+                appKey + from + to + "&health=alcohol-free" + calories + time + exluded + diet;
+        return result;
+    }
+
+}
+
 public class DataExtraction {
     private Gson gson = new Gson();
     private String recipeAppID = "ab5047e9";
@@ -41,28 +100,11 @@ public class DataExtraction {
     private String foodAppKey = "109788009c2c1ca6ff5ac36a4698aa0a";
     private String requirement = "";
 
-    /*
-     * possible requirements:
-     * - parser?ingr=red%20apple    food search
-     * - search?q=chicken           recipe search
-     *
-     *
-     */
-
-
-    public static void main(String[] args) {
-        String jsonString = DataExtraction.sendRequest(null, null); // stub
-        JsonObject object = DataExtraction.jsonStringToObject(jsonString);
-    }
-
-    private class DummyClass {
-    }
-
-
-    public static String sendRequest(String userID, String appKey) {
+    // EFFECTS; send request Edamam with a URL, return Json String from server
+    public String sendRequest(String url) {
         String result = "";
         try {
-            URL urlForGetRequest = new URL("https://api.edamam.com/search?q=chicken&app_id=ab5047e9&app_key=391d21268595ea4650e3d0f9039af88f&from=0&to=3&health=alcohol-free&calories=591-722");
+            URL urlForGetRequest = new URL(url);
             String readLine = null;
             HttpURLConnection conection = (HttpURLConnection) urlForGetRequest.openConnection();
             conection.setRequestMethod("GET");
@@ -76,13 +118,10 @@ public class DataExtraction {
                 }
                 in.close();
 
-                // print result
-                System.out.println("JSON String Result " + response.toString());
-
-                result = response.toString();
-
+                System.out.println("JSON String Result " + response.toString()); // print result
+                result = response.toString();                                    // return the json string
             } else {
-                System.out.println("GET NOT WORKED");
+                System.out.println("GET NOT WORKED, YOUR URL IS NOT FORMATTED CORRECTLY");
             }
 
         } catch (Exception e) {
@@ -91,7 +130,8 @@ public class DataExtraction {
         return result;
     }
 
-    public static JsonObject jsonStringToObject(String jsonString) {
+    // EFFECTS: parse string to json object
+    public JsonObject jsonStringParser(String jsonString) {
         try {
             JsonReader jsonReader = new JsonReader(new StringReader(jsonString));
             Gson gson = new Gson();
@@ -102,51 +142,47 @@ public class DataExtraction {
         return null;
     }
 
-    private class recipeQuery {
-        private String q; // query text
-        private String r; // ID of recipe
-        private String appID = recipeAppID;
-        private String appKey = recipeAppKey;
-        private Integer from = 0;
-        private Integer to = 3;
-        private String diet = Diet.BALANCED.toString(); //one of “balanced”, “high-protein”, “high-fiber”, “low-fat”, “low-carb”, “low-sodium”
-        private String range = "100-300";
-        private String time;
-
-
-
-
-        private String toURL() {
-            String result = "https://api.edamam.com/search?q=chicken&app_id=ab5047e9&app_key=391d21268595ea4650e3d0f9039af88f&from=0&to=3&health=alcohol-free&calories=591-722";
-            return "";
+    // EFFECTS: return an array of object (recipes) from Edamam Search Result
+    public ArrayList<Object> extractHits(JsonObject object){
+        JsonArray hits = object.getAsJsonArray("hits");
+        ArrayList<Object> list = new ArrayList<Object>();
+        try {
+            for (int i=0, l=hits.size(); i<l; i++){
+                list.add(hits.get(i));
+            }
+        } catch (Exception e) {
+            System.out.println("extractHits(): ERROR");
         }
 
-        /* TIME
-         * Time range for the total cooking and prep time for a recipe .
-         * The format is time=RANGE where RANGE is replaced by the value in minutes.
-         * RANGE is in one of MIN+, MIN-MAX or MAX, where MIN and MAX are non-negative integer numbers.
-         * The + symbol needs to be properly encoded.
-         * Examples: “time=1%2B” will return all recipes with available total time greater then 1 minute*/
-
-
-        /* RANGE
-         * The format is calories=RANGE where RANGE is replaced by the value in kcal.
-         * RANGE is in one of MIN+, MIN-MAX or MAX, where MIN and MAX are non-negative
-         * integer numbers. The + symbol needs to be properly encoded.
-         * Examples: “calories=100-300” will return all recipes with which have
-         * between 100 and 300 kcal per serving.
-         * */
-
-        /* EXCLUDING
-         * Excluding recipes with certain ingredients.
-         * The format is excluded=FOOD where FOOD is replaced by the name of the specific
-         * food you don’t want to be present in the recipe results. More than one food can
-         * be excluded at the same time. Example: excluded=vinegar&excluded=pretzel will
-         * exclude any recipes which contain vinegar or pretzels in their ingredient list
-         * */
-
+        return list;
     }
 
+
+    // EFFECTS: given an URL, return an array of object (recipes) from Edamam
+    public ArrayList<Object> getEdamamRecipes(String url) {
+        String jsonString = sendRequest(url); // stub
+        JsonObject result = jsonStringParser(jsonString);
+        return extractHits(result);
+    }
+
+//    public static ArrayList<Object>
+
+    public static void main(String[] args) {
+
+        // --- Format Query, these are possible attributes
+        RecipeQuery query = new RecipeQuery("pork");
+        query.setCalories(100, 700);
+        query.setTime(10, 20);
+        query.setDiet(Diet.LOW_CARB.toString());  // //one of “balanced”, “high-protein”, “high-fiber”, “low-fat”, “low-carb”, “low-sodium”
+        String url = query.toURL(); // output query to url string
+
+
+        // --- HTTP REQUEST
+        DataExtraction dataExtraction = new DataExtraction();
+        ArrayList<Object> list = dataExtraction.getEdamamRecipes(url);
+
+        System.out.println(list.toString());
+    }
 
 
 }
