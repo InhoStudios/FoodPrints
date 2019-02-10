fff9b67864fb7b1b6a52f1effaae2bec81024b80
Andy Shen <andyshenproper@gmail.com>
2019-02-09 22:29:25 -0800

    Camera shit

--- FoodPrints/src/main/java/com/inhostudios/visionapitester/FoodPrints.java
+++ FoodPrints/src/main/java/com/inhostudios/visionapitester/FoodPrints.java
@@ -1,16 +1,20 @@
 package com.inhostudios.visionapitester;
 
+import com.inhostudios.visionapitester.Camera.Camera;
+
 import java.util.ArrayList;
 
 public class FoodPrints {
 
     public static void main(String[] args){
 
-        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\image.jpg";
+        String path = System.getProperty("user.dir") + "\\src\\main\\resources\\apple.jpg";
 
-        // webcam creator
-
-        // end of webcam shit
+        // camera
+        Camera camera = new Camera();
+        Thread cTh = new Thread(camera);
+        cTh.start();
+        // no more camera
 
         ImageInterpreter imageInterpreter = new ImageInterpreter(path);
         ArrayList<String> outputs = imageInterpreter.getOutputs();
