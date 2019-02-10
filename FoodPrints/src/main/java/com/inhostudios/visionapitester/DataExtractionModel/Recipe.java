32d96f5f06b853c90151c18f05df9b360a80bbf8
harrychuang <vrjgik5@gmail.com>
2019-02-09 22:33:08 -0800

    working on string parsing for recipe class

---
+++ FoodPrints/src/main/java/com/inhostudios/visionapitester/DataExtractionModel/Recipe.java
@@ -0,0 +1,20 @@
+package com.inhostudios.visionapitester.DataExtractionModel;
+
+public class Recipe {
+    private String calories;
+    private String fat;
+    private String protein;
+    private String carbs;
+    private String fibre;
+    private String directions;
+    private String cooktime;
+    private String urlToRecipe;
+
+
+
+    // TODO: take a list of json objects and return a list of Recipe Object
+    public Recipe[] extractListOfRecipe(Object[] objects){
+        return null; // stub
+    }
+
+}
